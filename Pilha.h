a4fef251519c5d029bcb5749b207e1d0a0a19ddc
Ros Cibely <Ros Cibely>
2017-02-14 22:14:17 -0200

    Header do Arquivo Pilha.c

--- Pilha.h
+++ Pilha.h
@@ -1,28 +1,28 @@
-typedef struct pilha Pilha;
-
-typedef struct lista Lista;
-
-struct lista{
-    int info;
-    struct lista * prox;
-};
-
-struct pilha{
-	Lista * prim;
-	int t;
-};
-
-Pilha * pilha_cria(void);
-
-void pilha_push(Pilha* p, int v);
-
-int pilha_pop(Pilha* p);
-
-void pilha_libera(Pilha*p);
-
-int vazia(Pilha *p);
-
-void imprime(Pilha*l);
-
-int topo(Pilha* p);
+	typedef struct pilha Pilha;
+	
+	typedef struct lista Lista;
+	
+	struct lista{
+	    int info;
+	    struct lista * prox;
+	};
+	
+	struct pilha{
+		Lista * prim;
+		int t;
+	};
+	
+	Pilha * pilha_cria(void);
+	
+	void pilha_push(Pilha* p, int v);
+	
+	int pilha_pop(Pilha* p);
+	
+	void pilha_libera(Pilha*p);
+	
+	int vazia(Pilha *p);
+	
+	void imprime(Pilha*l);
+	
+	int topo(Pilha* p);
 
